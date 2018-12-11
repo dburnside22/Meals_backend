3e4e8de8d4a08b58852c5ecc4db2ca86ce332a7e
Dan Burnside <dburnside@stmtires.com@Dans-MBP.attlocal.net>
2018-12-10 23:16:27 -0600

    added breafast, lunch and dinner food boolean fields to models

--- dishes/models.py
+++ dishes/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Dish(models.Model):
     name = models.CharField(max_length=200)
+    breakfast_food = models.BooleanField(default=False)
+    lunch_food = models.BooleanField(default=False)
+    dinner_food = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- veggies/models.py
+++ veggies/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Veggie(models.Model):
     name = models.CharField(max_length=200)
+    breakfast_food = models.BooleanField(default=False)
+    lunch_food = models.BooleanField(default=False)
+    dinner_food = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.name
